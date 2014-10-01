a2be6abceb6b9b78e4b400f00b0c0b306aa7bf62
Oktay <olegognanovminchev7@gmail.com>
2014-10-02 00:54:10 +0300

    scanNetwork
    scan the local network and displays all the machines' ip and mac
    adresses which are connected to .

---
+++ IPinfo/IPinfo/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace IPinfo
+{
+    public partial class Form1 : Form
+    {
+        //create a last scan informations and saving the previous scan informations in the list "LastInfo".
+        List<IPInfo> LastInfo = new List<IPInfo>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(new ThreadStart(GetResults));
+            t.Start();
+            label1.ForeColor = System.Drawing.Color.Red;
+            label1.Text = "Processing ...";
+            label1.Visible = true;
+            Application.DoEvents();
+        }
+
+        private void GetResults()
+        {
+            richTextBox1.Clear();
+
+            try
+            {
+                // a List for ip informations 
+                List<IPInfo> info = IPInfo.GetIPInfo();
+
+                // scan and display ip-mac-host informations and what is the machine status is "on" or "off".
+                // status "offline" is being checked with the current and previous scans.
+
+                foreach (IPInfo item in info)
+                {
+                    Application.DoEvents();
+                    richTextBox1.AppendText("IP: " + item.IPAddress + "\n");
+                    richTextBox1.AppendText("Mac: " + item.MacAddress + "\n");
+                    richTextBox1.AppendText("Host: " + item.HostName + "\n");
+                    richTextBox1.AppendText("Status: Online\n");
+                    richTextBox1.AppendText("-----------------------------------------------");
+                    richTextBox1.AppendText("\n");
+                }
+
+                richTextBox1.AppendText("\n");
+
+                foreach (IPInfo item in LastInfo)
+                {
+                    if (info.Where(i => i.IPAddress == item.IPAddress).Count() == 0)
+                    {
+                        Application.DoEvents();
+                        richTextBox1.AppendText("IP: " + item.IPAddress + "\n");
+                        richTextBox1.AppendText("Mac: " + item.MacAddress + "\n");
+                        richTextBox1.AppendText("Host: " + item.HostName + "\n");
+                        richTextBox1.AppendText("Status: Offline\n");
+                        richTextBox1.AppendText("-----------------------------------------------");
+                        richTextBox1.AppendText("\n");
+                    }
+                }
+
+                LastInfo = new List<IPInfo>(info.ToArray());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An error-Naptın ulen dumbass !");
+            }
+
+            label1.ForeColor = System.Drawing.Color.Green;
+            label1.Text = "Completed ! Scan is over, wanna give a new try?";
+
+        }
+    }
+}
---
+++ IPinfo/IPinfo/IPinfo.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// for execute arp -a call , system.diagnostics.process is needed!
+using System.Diagnostics;
+// for host names , system.net is needed.
+using System.Net;
+
+namespace IPinfo
+{
+    public class IPInfo
+    {
+        public IPInfo(string macAddress, string ipAddress)
+        {
+            this.MacAddress = macAddress;
+            this.IPAddress = ipAddress;
+        }
+
+        public string MacAddress { get; private set; }
+        public string IPAddress { get; private set; }
+
+        private string _HostName = string.Empty;
+        public string HostName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this._HostName))
+                {
+                    try
+                    {
+                        // display the "Host Name" for this IP Address. "Name" of the machine.
+                        this._HostName = Dns.GetHostEntry(this.IPAddress).HostName;
+                    }
+                    catch
+                    {
+                        this._HostName = string.Empty;
+                    }
+                }
+                return this._HostName;
+            }
+        }
+
+
+        #region "Static Methods"
+        
+        /// displays the IPInfo for machines on the local network with the MAC Address.
+        /// 
+
+        public static IPInfo GetIPInfo(string macAddress)
+        {
+            var ipinfo = (from ip in IPInfo.GetIPInfo()
+                          where ip.MacAddress.ToLowerInvariant() == macAddress.ToLowerInvariant()
+                          select ip).FirstOrDefault();
+
+            return ipinfo;
+        }
+
+       
+        /// displays the IPInfo for machines on the local network.
+        
+        public static List<IPInfo> GetIPInfo()
+        {
+            try
+            {
+                var list = new List<IPInfo>();
+
+                foreach (var arp in GetARPResult().Split(new char[] { '\n', '\r' }))
+                {
+                    //  all the MAC / IP Address combinations
+                    if (!string.IsNullOrEmpty(arp))
+                    {
+                        var pieces = (from piece in arp.Split(new char[] { ' ', '\t' })
+                                      where !string.IsNullOrEmpty(piece)
+                                      select piece).ToArray();
+                        if (pieces.Length == 3)
+                        {
+                            list.Add(new IPInfo(pieces[1], pieces[0]));
+                        }
+                    }
+                }
+
+                // Return list of IPInfo objects MAC / IP Address infos
+                return list;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("IPInfo: Error 'arp -a' results", e);
+            }
+        }
+
+        /// This runs the "arp"  in Windows to find all the MAC / IP Addresses.
+        
+        private static string GetARPResult()
+        {
+            Process p = null;
+            string output = string.Empty;
+
+            try
+            {
+                p = Process.Start(new ProcessStartInfo("arp", "-a")
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                    
+                });
+
+                output = p.StandardOutput.ReadToEnd();
+                
+                p.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("IPInfo: Error display 'arp -a' Results", ex);
+            }
+            finally
+            {
+                if (p != null)
+                {
+                    p.Close();
+                }
+            }
+
+            return output;
+        }
+
+        #endregion
+    }
+}
